018ea98b4cfa34d74d2c7ef059267558044344c0
Nihal SANDHU <nihalsandhu007220@gmail.com>
2023-11-17 20:07:12 +0530

    operatos part 2 first question done

---
+++ Front-End/Js/Assignments/operatorsinJsPart2/1stquestion.js
@@ -0,0 +1,19 @@
+// Q1 Explain the usage of ternary operator with syntax and write a program to check whether the number is even
+// or odd using the ternary operator.
+
+
+// The ternary operator, also known as the conditional operator, is a concise way to write conditional statements in many programming languages. It's a shorthand way of expressing an if-else statement.
+
+// syntax of ternory operator 
+
+// condition ? expression1 : expression2
+
+// Here, if the condition is true, expression1 is executed; otherwise, expression2 is executed.
+
+
+// to check number is even or odd here is the code for ThaT 
+
+let number = 15 
+
+ let result = number % 2 == 0 ? "even ":" odd"
+console.log(result)
