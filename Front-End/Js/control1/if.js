4cb5a41339c55922e0d649c78bed99f3863b5f8d
Nihal SANDHU <nihalsandhu007220@gmail.com>
2023-11-19 15:59:01 +0530

    if statement

---
+++ Front-End/Js/control1/if.js
@@ -0,0 +1,24 @@
+let userLOgged = false;
+
+if(userLOgged){
+    console.log(`user has been logged`)
+}else{
+    console.log(`Loggin  has failed`)
+}
+
+
+let score = 200
+
+if(score > 100){
+    console.log('score has been good')
+    score -= 100
+}else if(score == 100){
+console.log(`score has been lower to 100`)
+}
+console.log(score)
+
+
+
+if(""){  // empty string is falsy value
+console.log('hello')
+}
