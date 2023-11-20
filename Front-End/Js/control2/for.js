cc6344178158daef2b870e132facd407878f3f66
Nihal SANDHU <nihalsandhu007220@gmail.com>
2023-11-20 23:44:53 +0530

    loops

---
+++ Front-End/Js/control2/for.js
@@ -0,0 +1,16 @@
+// lets start loops we can call it iteration 
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element)
+// }
+
+// lets print number that are divisble by 2 only 
+
+for(let i = 1 ; i<10;i++){
+    if(i % 2 ==0){
+        console.log(i)
+    }
+}
+
+// here it is 2,4,6,8 will be printed 
